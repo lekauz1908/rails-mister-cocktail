ab4e67b48aef17efab2789f563b5cc9ff3cb8a58
marc q <marc.quitschau@gmail.com>
2020-05-22 12:56:33 +0200

    structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,10 @@ puts "Creating ingredients..."
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "bitter")
+Ingredient.create(name: "orange")
+Ingredient.create(name: "basil leaves")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "water")
+Ingredient.create(name: "sugar")
 puts "ingredients created ..."
